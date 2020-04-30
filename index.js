b2240ffa5d1c60fa7970d349574ab84cab91de8c
tkreind <tristankreindler@gmail.com>
2020-04-30 17:17:27 -0400

    Switched to joey-bot

--- index.js
+++ index.js
@@ -1,10 +1,79 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
+const https = require('https')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = parseInt(process.env.PORT) || 5000
+const BOT_ID = process.env.BOT_ID
+const ROOM_ID = process.env.ROOM_ID
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+
+if (!BOT_ID) {
+	console.log("Missing BOT_ID environment variable.");
+}
+if (!ROOM_ID) {
+	console.log("Missing ROOM_ID environment variable.");
+}
+if (!ACCESS_TOKEN) {
+	console.log("Missing ACCESS_TOKEN environment variable.");
+}
+if (!BOT_ID || !ROOM_ID || !ACCESS_TOKEN) {
+	process.exit(1);
+}
+
+// main express server
+const app = express();
+app.use(bodyParser.json())
+app.post('/', (req, res) => handle(req, res));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+const command = "/joey";
+
+handle = (req, res) => {
+
+	if (!req.body || !req.body.text) {
+		res.status(400).json({body: false});
+		return;
+	}
+
+	if (req.body.text.substr(0, command.length).toLowerCase() !== command) {
+		res.status(200).json({status:"ok"})
+		return;
+	}
+
+	const text = "";
+	const message = {
+		text,
+		BOT_ID,
+		attachments: [
+			{
+				"type": "image",
+				"url": "https://i.groupme.com/somethingsomething.large"
+			}
+		]
+	};
+
+	// Send the request
+	const json = JSON.stringify(message);
+	const groupmeAPIOptions = {
+		agent: false,
+		host: "api.groupme.com",
+		path: "/v3/bots/post",
+		port: 443,
+		method: "POST",
+		headers: {
+			"Content-Length": json.length,
+			"Content-Type": "application/json",
+			"X-Access-Token": ACCESS_TOKEN
+		}
+	};
+	const request = https.request(groupmeAPIOptions, response => {
+		let data = "";
+		response.on("data", chunk => (data += chunk));
+		response.on("end", () =>
+			console.log(`[GROUPME RESPONSE] ${response.statusCode} ${data}`)
+		);
+	});
+	request.end(json);
+
+	res.status(200).json({status:"ok"})
+}
--- test.js
+++ test.js
@@ -1,28 +1,59 @@
 const { spawn } = require('child_process');
 const request = require('request');
 const test = require('tape');
+const fs = require('fs');
+
+const PORT = 5000;
+
+// read hidden tokens as json
+const tokens = JSON.parse(fs.readFileSync('tokens.json'));
 
 // Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
+
+// add default port of 5000 to environment
+let env = Object.assign({}, process.env, { PORT: PORT });
+
+// add hidden tokens to tthe environment
+env = Object.assign({}, env, tokens);
+
+// spawn the server with custom environment
+const child = spawn('node', ['index.js'], { env });
 
 test('responds to requests', (t) => {
-  t.plan(4);
+	t.plan(2);
 
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
-      // stop the server
-      child.kill();
+	// Wait until the server is ready
+	child.stdout.on('data', _ => {
 
-      // No error
-      t.false(error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started on Heroku with Node.js"), -1);
-    });
-  });
+		const options = {
+			uri: 'http://127.0.0.1:5000',
+			method: 'POST',
+			json: {
+				"attachments": [],
+				"avatar_url": "https://i.groupme.com/123456789",
+				"created_at": 1302623328,
+				"group_id": "1234567890",
+				"id": "1234567890",
+				"name": "John",
+				"sender_id": "12345",
+				"sender_type": "user",
+				"source_guid": "GUID",
+				"system": false,
+				"text": "/joey",
+				"user_id": "1234567890"  
+			}
+		};
+
+		request(options, (error, res, body) => {
+
+				// stop the server
+				child.kill();
+				
+				// No error
+				t.false(error);
+				// Successful response
+				t.equal(res.statusCode, 200);
+			}
+		)
+	});
 });
